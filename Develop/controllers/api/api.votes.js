48c5eb7a2a5d80b87a2843ac0dae69edaf9c272b
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2023-01-07 17:23:19 -0600

    supported for games.name so it actually works

--- Develop/controllers/api/api.votes.js
+++ Develop/controllers/api/api.votes.js
@@ -6,15 +6,26 @@ const db = require("../models");
 
 router.post("/api/votes", (req, res) => {
 	const { gameId, userId, score } = req.body;
-	db.user_votes
-		.create({
-			game_id: gameId,
-			user_id: userId,
-			score,
+	db.games
+		.findOne({
+			where: { id: gameId },
+			attributes: ["name"],
 		})
-		.then((vote) => {
-			// OPTIONAL: sends the updated score for the game as a response if the vote was properly written to the db
-			res.json({ score: vote.score });
+		.then((game) => {
+			db.user_votes
+				.create({
+					game_id: gameId,
+					user_id: userId,
+					score,
+					game: game.name,
+				})
+				.then((vote) => {
+					// OPTIONAL: sends the updated score for the game as a response if the vote was properly written to the db
+					res.json({ score: vote.score });
+				})
+				.catch((err) => {
+					res.status(500).json({ message: err.message });
+				});
 		})
 		.catch((err) => {
 			res.status(500).json({ message: err.message });
