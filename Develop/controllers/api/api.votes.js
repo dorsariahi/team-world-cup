2f1123af02aa17a5d12ec02c02cc67694cb57da9
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2023-01-07 16:42:34 -0600

    add post request

--- Develop/controllers/api/api.votes.js
+++ Develop/controllers/api/api.votes.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const apiRouter = express.Router();
+const router = express.Router();
 const { application } = require("express");
 const sequelize = require("../../config/connection");
+const db = require("../models");
 
-apiRouter.get("/api/games", (req, res) => {
-	getGames();
+router.post("/api/votes", (req, res) => {
+	const { gameId, userId, score } = req.body;
+	db.user_votes
+		.create({
+			game_id: gameId,
+			user_id: userId,
+			score,
+		})
+		.then((vote) => {
+			// OPTIONAL: sends the updated score for the game as a response if the vote was properly written to the db
+			res.json({ score: vote.score });
+		})
+		.catch((err) => {
+			res.status(500).json({ message: err.message });
+		});
 });
+
+module.exports = router;
